bca0360bb6950b8b225f429f4492fb43bc511286
James-Burch <burchjames6@gmail.com>
2024-10-18 03:49:03 +0100

    Save current changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name='profile'),
     path('password_change/', views.password_change, name='password_change'),
-    path('password_reset/', views.password_reset, name='password_reset'),
-    path('password_reset/done/', views.password_reset_done, name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', views.password_reset_confirm, name='password_reset_confirm'),
-    path('reset/done/', views.password_reset_complete, name='password_reset_complete'),
+    # path('password_reset/', views.password_reset, name='password_reset'),
+    # path('password_reset/done/', views.password_reset_done, name='password_reset_done'),
+    # path('reset/<uidb64>/<token>/', views.password_reset_confirm, name='password_reset_confirm'),
+    # path('reset/done/', views.password_reset_complete, name='password_reset_complete'),
    ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout, update_session_auth_hash
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -41,3 +41,20 @@ def logout_view(request):
 
 def profile(request):
     return render(request, 'accounts/profile.html')
+
+@login_required
+def password_change(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('profile')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'accounts/password_change.html', {
+        'form': form
+    })
