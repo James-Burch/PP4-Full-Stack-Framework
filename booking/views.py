3291bb492740bb8adfc719573a98bec8a24233d3
James Burch <burchjames6@gmail.com>
2024-10-23 01:09:25 +0000

    Add login/logout views to booking app

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 
@@ -19,3 +19,24 @@ def register(request):
         form = UserCreationForm()
 
     return render(request, 'booking/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'booking/login.html', {'form': form})  
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
