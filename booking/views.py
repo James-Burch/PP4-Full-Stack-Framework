9846004336bd9e32fd9c5c3417434870038fe296
James Burch <burchjames6@gmail.com>
2024-10-23 01:01:44 +0000

    Add a view for registering an account

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def my_booking(request):
     return HttpResponse('My Booking')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'booking/register.html', {'form': form})
